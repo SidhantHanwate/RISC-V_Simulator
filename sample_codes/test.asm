9eba2625b2f9e37130d252168f1a7620829f3d53
SIDHANT HANWATE <91300868+cs20b041@users.noreply.github.com>
2022-05-08 19:23:51 +0530

    Update test.asm

--- sample_codes/test.asm
+++ sample_codes/test.asm
@@ -1,13 +1,13 @@
-li x5, 5
-li x29, 3
-li x28, 1
+li x7, 100
+li x4, 10
+li x1, 5
+li x8, 2
 
-LABEL1: 
-add x5 , x5 , x28
-sub x5, x5 , x28
-bne x5, x29 , LABEL #branch instruction
+add x3, x2, x1
+add x3, x3, x3
+bne x3, x4, loop
+
+loop:
+addi x5, x4, 1
+add x5, x5, x5
 end
-
-LABEL: 
-sub x5, x5 , x28
-jal x5, LABEL1 
